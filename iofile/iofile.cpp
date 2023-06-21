d47529e5c79a66f6cba77527f17e9baef6c669a4
namira121 <127211008+namira121@users.noreply.github.com>
2023-06-21 10:26:04 +0700

    loop untuk menulis di dalam file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -13,4 +13,10 @@ int main() {
 	outfile.open("contohfile.txt");
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+
+	}
 }
