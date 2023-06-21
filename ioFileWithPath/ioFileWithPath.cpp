67ca338498d5e72cf0ee3ebc943b649ec5a686db
namira121 <127211008+namira121@users.noreply.github.com>
2023-06-21 10:57:49 +0700

    menggunakan mode menulis ofstream dan menunjuk ke sbuah nama file. Menggunkan ios::out untuk menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -9,4 +9,11 @@ int main() {
 
 	cout << "Masukkan Nama File : ";
 	cin >> NamaFile; 
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file 
+	outfile.open(NamaFile + ".txt", ios::out);
+
+
 }
